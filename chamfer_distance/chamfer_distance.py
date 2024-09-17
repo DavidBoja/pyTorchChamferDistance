54c8086b6008e87e5fefff48a0f247855a9a3b7e
DavidBoja <david.bojanic@hotmail.com>
2024-09-17 13:08:55 +0200

    Fixed bug.

--- chamfer_distance/chamfer_distance.py
+++ chamfer_distance/chamfer_distance.py
@@ -35,7 +35,7 @@ class ChamferDistanceFunction(torch.autograd.Function):
         return dist1, dist2, idx1, idx2
 
     @staticmethod
-    def backward(ctx, graddist1, graddist2):
+    def backward(ctx, graddist1, graddist2, idx1, idx2):
         xyz1, xyz2, idx1, idx2 = ctx.saved_tensors
 
         graddist1 = graddist1.contiguous()
